a0cdacd7da2520d97f2c5308c3d8ea969326e31f
Jacek Galowicz <jacek@galowicz.de>
2019-06-04 05:02:56 +0200

    Add cheap CA script

---
+++ ca.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p easyrsa
+
+set -eu
+
+easyrsa init-pki
+easyrsa build-ca nopass
+easyrsa gen-dh
+easyrsa build-server-full openvpn_server nopass
+easyrsa build-client-full openvpn_client1 nopass
+easyrsa build-client-full openvpn_client2 nopass
