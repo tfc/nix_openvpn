4fe99059df91780329fb650667204d4a1ed3275e
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2019-06-04 18:50:50 +0200

    Also generate SSH keypair with ca.sh

--- ca.sh
+++ ca.sh
@@ -9,3 +9,6 @@ easyrsa gen-dh
 easyrsa build-server-full openvpn_server nopass
 easyrsa build-client-full openvpn_client1 nopass
 easyrsa build-client-full openvpn_client2 nopass
+
+mkdir -p ssh_keys
+ssh-keygen -t rsa -f ssh_keys/buildfarm -q -N ""
